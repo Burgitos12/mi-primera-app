bbb6f1060335a903aa66f8892540e94b4aff4c45
Stiven Burgos <wilsonburgosx001@gmail.com>
2025-04-22 11:26:18 -0500

    Cambio de logo y nombre

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import logosena from './assets/sena.png';
 import './App.css';
 import Saludo from './componente/saludo'; // Asegúrate que esta ruta esté bien (componente en minúscula)
 
@@ -10,41 +9,36 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+        {/* Mostrar el logo */}
+        <img src={logosena} alt="Logo SENA" className="logo" />
 
-      <h1>Vite + React</h1>
+        <h1>mi primera app</h1>
 
-                {/* Incluir el saludo*/}
-      <Saludo nombre="Stiven Burgos" dia="Martes" />
+        {/* Incluir el saludo */}
+        <Saludo nombre="Stiven Burgos" dia="Martes" />
 
-    
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            + sumar
+          </button>
+          <button onClick={() => setCount((count) => count - 1)}>
+            - restar
+          </button>
+          <p>
+            Valor actual: <strong>{count}</strong>
+          </p>
+          <p>
+            Edita <code>src/App.jsx</code> y guarda para probar HMR
+          </p>
+        </div>
 
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          + sumar
-        </button>
-        <button onClick={() => setCount((count) => count - 1)}>
-          - restar
-        </button>
-        <p>
-          Valor actual: <strong>{count}</strong>
-        </p>
-        <p>
-          Edita <code>src/App.jsx</code> y guarda para probar HMR
+        <p className="read-the-docs">
+          Haz clic en los logos de Vite y React para saber más
         </p>
       </div>
-
-      <p className="read-the-docs">
-        Haz clic en los logos de Vite y React para saber más
-      </p>
     </>
   );
 }
 
+// Asegúrate de que esta línea esté al final del archivo, fuera de cualquier función
 export default App;
